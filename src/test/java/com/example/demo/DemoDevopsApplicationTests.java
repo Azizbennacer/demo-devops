6a64130a1fabf2f442792d1a812e16461730bf2b
Azizbennacer <azizbennacer9@gmail.com>
2026-01-10 23:01:21 +0100

    changes in DemoDevopsApplicationTests

--- src/test/java/com/example/demo/DemoDevopsApplicationTests.java
+++ src/test/java/com/example/demo/DemoDevopsApplicationTests.java
@@ -1,8 +1,9 @@
 package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest
+@ActiveProfiles("test")
 class DemoDevopsApplicationTests {
 
 	@Test
